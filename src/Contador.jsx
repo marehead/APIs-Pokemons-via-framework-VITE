77fb78a60cc44f2e7059b0301020d9265c7f7a99
Marina Lopes <96207587+marehead@users.noreply.github.com>
2022-11-26 11:21:45 -0300

    Update Contador.jsx

--- src/Contador.jsx
+++ src/Contador.jsx
@@ -1,1 +1,25 @@
-backgroundImage: `url(${contador.png})`
+import react, { useState } from "react";
+import Palpiteiro from "./Palpiteiro";
+
+const ContadorPalpiteiro = () => {
+  const [numero, setNumero] = useState(0);
+
+  const somarUm = () => {
+    setNumero((valor) => valor + 1);
+  };
+
+  const subtrairUm = () => {
+    setNumero((valor) => valor - 1);
+  };
+
+  return (
+    <>
+      <button onClick={somarUm}>+</button>
+      <p>{numero}</p>
+      <button onClick={subtrairUm}>-</button>
+      <br />
+      <Palpiteiro  />
+    </>
+  );
+};
+export default ContadorPalpiteiro;
